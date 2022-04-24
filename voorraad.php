72da021fe7f96540443c13264ef4f40fdf5ae9c0
KyaraDeWinter <70946292+KyaraDeWinter@users.noreply.github.com>
2022-04-24 22:45:52 +0200

    styling

--- voorraad.php
+++ voorraad.php
@@ -14,29 +14,43 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <link rel="stylesheet" href="css/stylesheet.css">
+
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
     <title>Voorraadsysteem</title>
   </head>
   <body>
-    <h1>Voorraadsysteem</h1>
-    <div class="card" style="width: 18rem;">
-        <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">
-                <?php echo $product1; ?>
-            </p>
+    <container>
+        <h1 class="title">Voorraadsysteem</h1>
+        <div class="card">
+            <div class="card-body">
+                <p class="card-text">
+                    <?php echo $product1; ?>
+                </p>
+            </div>
         </div>
-    </div>
-    <div class="card" style="width: 18rem;">
-        <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">
-                <?php echo $product2; ?>
-            </p>
+
+        <div class="card">
+            <div class="card-body">
+                <p class="card-text">
+                    <?php echo $product2; ?>
+                </p>
+            </div>
         </div>
-    </div>
+
+        <div class="card" style="margin-bottom: 2rem;">
+            <div class="card-body">
+                <p class="card-text">
+                    <?php
+                        echo $product1->Totaal();
+                        echo $product2->Totaal();
+                    ?>
+                </p>
+            </div>
+        </div>
+    </container>
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>

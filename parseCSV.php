9a0f01baea3149c7fc4058a71c8ed18dfae41018
KyaraDeWinter <70946292+KyaraDeWinter@users.noreply.github.com>
2022-04-24 20:40:02 +0200

    Update parseCSV.php

--- parseCSV.php
+++ parseCSV.php
@@ -1,5 +1,31 @@
 <?php
+class ParseCSV {
+    public $filenaam;
+    private $header;
+    private $data=[];
 
+    public funtion __construct($filenaam='') {
+        if($filenaam != '') {
+            $this->filenaam = $filenaam;
+        }
+    }
+
+    public function parse() {
+        $file - fopen($this->filenaam, 'r');
+        while(!feaf($file)) {
+            $row = fgetcsv($file, 0, ',');
+            if($row == [NULL] || $row === FALSE) { continue;}
+            if(!$this->header) {
+                $this->header = $row;
+            } else {
+              $this->data[] = array_combine($this->header, $row);
+            }
+        }
+        fclose($file);
+
+        return $this->data;
+    }
+}
 
 
 ?>
